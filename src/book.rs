40d02ba1d4788766de44dece1b6c0b8d0b0377f6
Matheus Gonçalves <matheus.goncalves@alumni.usp.br>
2021-12-31 00:32:34 -0300

    book print xml

--- src/book.rs
+++ src/book.rs
@@ -1,8 +1,27 @@
+use quick_xml::Reader;
+use quick_xml::events::Event;
+use std::io::BufReader;
+use std::io::Read;
+
 pub struct Book {
 
 }
 
 impl Book {
-    pub open(&self, reader: BufReader) -> self {
+    pub fn open<R: Read>(reader: BufReader<R>) {
+        let mut reader = Reader::from_reader(reader);
+        let mut buf = Vec::new();
+
+        loop {
+            match reader.read_event(&mut buf) {
+                Ok(Event::Start(ref e)) => {
+                    println!("{:?}", std::str::from_utf8(e.name()).unwrap());
+                }
+                Ok(Event::Eof) => break,
+                Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+                _ => (),
+            }
+
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,12 @@
 use std::fs::File;
 use std::io::BufReader;
-use std::io::copy;
 use flate2::read::GzDecoder;
 
+mod book;
 
-pub fn open(path: &str) -> Result<(), std::io::Error>  {
-    let gnucash = File::open(path)?;
-    let mut xml = BufReader::new(GzDecoder::new(gnucash));
+pub fn open(path: &str) {
+    let gnucash = File::open(path).unwrap();
+    let xml = BufReader::new(GzDecoder::new(gnucash));
 
-    let mut gnucash_xml = File::create("/home/matheus/workspace/gnucash-lib/examples/account.xml")?;
-
-    let _ = copy(&mut xml, &mut gnucash_xml);
-
-    Ok(())
+    book::Book::open(xml)
 }
